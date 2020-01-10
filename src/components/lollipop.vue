c7da79e3e2b79b869b46850527f2dcc569f2c14e
ewice <henning.loewe@hotmail.de>
2020-01-10 10:05:46 +0100

    add lollipop.vue

---
+++ src/components/lollipop.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="container" style="width:100%; height:400px;"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'lollipop',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      options: {
+        paddingTop: '30px'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
