80e5de8e34923ea0e39b45bddd170ea2085169f0
ewice <henning.loewe@hotmail.de>
2020-01-11 15:24:12 +0100

    delete highcharts map

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueFullPage from 'vue-fullpage.js'
 import BootstrapVue from 'bootstrap-vue'
-import Highcharts from 'highcharts'
 import HighchartsVue from 'highcharts-vue'
-import loadMap from 'highcharts/modules/map.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -13,7 +11,6 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(VueFullPage)
 Vue.use(HighchartsVue)
-loadMap(Highcharts)
 
 new Vue({
   render: h => h(App)
